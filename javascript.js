620b71bdef95bfb63595d8a133c97cc062a9062c
Asjas <asjas@outlook.com>
2017-09-22 20:56:14 +0200

    Initial js file. Still to to work on it

---
+++ javascript.js
@@ -0,0 +1,15 @@
+jQuery( function( $ ) {
+  $( '#another-quote' ).on( 'click', function ( e ) {
+    e.preventDefault();
+    $.ajax( {
+      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      success: function ( data ) {
+        var post = data.shift(); // The data is an array of posts. Grab the first one.
+        $( '#quote-title' ).text( post.title );
+        $( '#quote-content' ).html( post.content );
+        $( '#twitter').href( post.Link );
+      },
+      cache: false
+    } );
+  } );
+} );
