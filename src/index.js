265066d8a8b74b918e2b4eec13698d3d1d6d3799
Asjas <asjas@outlook.com>
2019-09-19 17:41:01 +0200

    Update main index and public html files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { redA400 } from 'material-ui/styles/colors';
-import App from './components/App.js';
-import setupLogRocket from './utils/logrocket';
 import * as serviceWorker from './serviceWorker';
-import 'normalize.css';
+
 import './styles/index.css';
 import './styles/media.css';
+
+import App from './components/App.js';
+
+import setupLogRocket from './utils/logrocket';
+
+setupLogRocket();
 
 const muiTheme = getMuiTheme({
   palette: {
@@ -23,7 +27,6 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-setupLogRocket();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
