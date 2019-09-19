2a1aa484d985dc2e6f5c4f6b943930d366fcb44d
Asjas <asjas@outlook.com>
2019-09-19 17:43:02 +0200

    Add prettier and update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,14 +11,17 @@ module.exports = {
   },
   env: {
     browser: true,
-    node: true,
     jest: true,
   },
   rules: {
     'no-debugger': 0,
     'no-alert': 0,
-    'arrow-parens': 0,
-    'no-unused-expressions': 0,
+    'no-unused-vars': [
+      1,
+      {
+        argsIgnorePattern: 'res|next|^err',
+      },
+    ],
     'prefer-const': [
       'error',
       {
@@ -26,6 +29,15 @@ module.exports = {
       },
     ],
     'arrow-body-style': [2, 'as-needed'],
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'operator-linebreak': 0,
+    'no-unused-expressions': [
+      2,
+      {
+        allowTaggedTemplates: true,
+      },
+    ],
     'no-param-reassign': [
       2,
       {
@@ -36,22 +48,23 @@ module.exports = {
     'import/prefer-default-export': 0,
     import: 0,
     'func-names': 0,
-    'import/no-extraneous-dependencies': 0,
+    'no-use-before-define': 0,
     'space-before-function-paren': 0,
     'comma-dangle': 0,
+    'global-require': 0,
+    'no-shadow': 0,
     'max-len': 0,
     'import/extensions': 0,
     'no-underscore-dangle': 0,
     'consistent-return': 0,
+    'react/display-name': 1,
     'react/no-array-index-key': 0,
-    'prefer-arrow-callback': 0,
     'react/react-in-jsx-scope': 0,
     'react/prefer-stateless-function': 0,
+    'react/require-default-props': 0,
     'react/forbid-prop-types': 0,
-    'react/destructuring-assignment': 0,
-    'react/no-danger': 0,
-    'react/jsx-one-expression-per-line': 0,
     'react/no-unescaped-entities': 0,
+    'react/sort-comp': 0,
     'react/jsx-max-props-per-line': [
       2,
       {
@@ -63,17 +76,10 @@ module.exports = {
     'react/jsx-filename-extension': [
       1,
       {
-        extensions: ['.js'],
+        extensions: ['.js', '.jsx'],
       },
     ],
     radix: 0,
-    'no-shadow': [
-      2,
-      {
-        hoist: 'all',
-        allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
-      },
-    ],
     quotes: [
       2,
       'single',
